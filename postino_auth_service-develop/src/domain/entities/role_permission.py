1742e777d301828bbc70903ee1c100f2fa6a3b2e
Ashkan <ashkan.sharifi1933@gmail.com>
2025-07-25 17:50:38 +0330

    Delete postino_auth_service-develop directory

--- postino_auth_service-develop/src/domain/entities/role_permission.py
+++
@@ -1,9 +0,0 @@
-import uuid
-from dataclasses import dataclass
-
-
-# ----------------------------------------------------------------------------
-@dataclass
-class RolePermission:
-    role_id: uuid.UUID
-    permission_id: uuid.UUID
--- postino_auth_service-develop/src/domain/interfaces/notify_user.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-# ----------------------------------------------------------------------------
-class INotifyUser(ABC):
-    @abstractmethod
-    def send_request(self, phone_number: str, otp: str): ...
